429eb69bb665c1646e9ac6bfe18a132e85ae4710
ArpMichaelB <40207147+ArpMichaelB@users.noreply.github.com>
2019-01-15 14:33:04 -0600

    Gitignore+Basic Structure
    The basic structure of my portion of the website, and a gitignore that just removes every part of the project except for the actual source files that specify the build of the website.

--- yashadvert/src/pages/AboutGTP/AboutGTP.js
+++ yashadvert/src/pages/AboutGTP/AboutGTP.js
@@ -9,6 +9,65 @@ const AboutGTP = (props, context) => {
         <main className="about_gtp_content">
             <div className="title">About Yash GTP</div>
             {/* the about div is gonna go here, full of paragraph tags */}
+            <div className="about_gtp_heading">
+                Technologies
+            </div>
+            <div className="about_gtp_list">
+                <ul>
+                    <li>
+                        Spring
+                        <ul>
+                            <li>
+                                Rest
+                            </li>
+                            <li>
+                                JDBC
+                            </li>
+                            <li>
+                                MVC
+                            </li>
+                        </ul>
+                    </li>
+                    <li>
+                        Amazon Web Services
+                    </li>
+                    <li>
+                        Hibernate
+                    </li>
+                    <li>
+                        JUnit
+                    </li>
+                    <li>
+                        ReactJS
+                    </li>
+                    <li>
+                        NodeJS
+                    </li>
+                    <li>
+                        Servlets
+                    </li>
+                    <li>
+                        JSP
+                    </li>
+                </ul>
+            </div>
+            <div className="about_gtp_heading">
+                Specifics
+            </div>
+            <div className="about_gtp_paragraph">
+                <p>
+                    The Graduate Training Program at Yash Technologies focuses on both contemporary and upcoming technologies. 
+                    <br/>
+                    As an example, the second batch of trainees (the workers who initially designed this site) had a training focusing heavily on Java libraries, such as Spring, (majority Rest and JDBC), AWS, JUnit, and Hibernate. 
+                    <br/>
+                    Additionally, there was some focus on other non-Java technologies, including (but not limited to) ReactJS, NodeJS, and frontend technologies like Java Servlets, JSP pages, and Spring MVC.
+                </p>
+            </div>
+            <div className="upcoming_gtp_paragraph">
+                <p>
+                    Upcoming trainees should expect a much more precise focus on .Net, and by extension, Microsoft Azure, rather than the earlier training on Amazon Web Services.
+                </p>
+            </div>
         </main>
     )
 }
