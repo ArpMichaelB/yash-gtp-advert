c5be73fb16473e8cb2fb955c662cac6a7c40d275
Zachary Karamanlis <zachary.karamanlis@gmail.com>
2019-01-16 14:17:27 -0600

    Basic layout

--- yashadvert/src/components/Footer/Footer.js
+++ yashadvert/src/components/Footer/Footer.js
@@ -4,96 +4,19 @@ import './style.css';
 
 const Footer = (props, context) => {
     return (
-<section class="footer">
-<div class="container">
- <div class="row">
-  <div class="col-sm-6">
-    <div class="f_menu">
-        <h4> Quick Links </h4>
-        <ul id="menu-quick-links" class="row"><li id="menu-item-6247" class="col-sm-6 menu-item menu-item-type-post_type menu-item-object-page menu-item-6247"><a href="https://www.yash.com/digital-transformation/">Digital Transformation</a></li>
-<li id="menu-item-6248" class="col-sm-6 menu-item menu-item-type-post_type menu-item-object-page menu-item-6248"><a href="https://www.yash.com/digital-transformation/big-data/">Big Data</a></li>
-<li id="menu-item-6249" class="col-sm-6 menu-item menu-item-type-post_type menu-item-object-page menu-item-6249"><a href="https://www.yash.com/digital-transformation/analytics/">Data Sciences &amp; Analytics</a></li>
-<li id="menu-item-6250" class="col-sm-6 menu-item menu-item-type-post_type menu-item-object-page menu-item-6250"><a href="https://www.yash.com/digital-transformation/cloud/">Cloud Services</a></li>
-<li id="menu-item-6251" class="col-sm-6 menu-item menu-item-type-post_type menu-item-object-page menu-item-6251"><a href="https://www.yash.com/digital-transformation/mobility-services/">Mobility Services</a></li>
-<li id="menu-item-6252" class="col-sm-6 menu-item menu-item-type-post_type menu-item-object-page menu-item-6252"><a href="https://www.yash.com/it-outsourcing/managed-services/">Managed Services</a></li>
-<li id="menu-item-6253" class="col-sm-6 menu-item menu-item-type-post_type menu-item-object-page menu-item-6253"><a href="https://www.yash.com/it-outsourcing/testing/">Testing Services</a></li>
-<li id="menu-item-6254" class="col-sm-6 menu-item menu-item-type-post_type menu-item-object-page menu-item-6254"><a href="https://www.yash.com/it-outsourcing/infrastructure-management/infrastructure-services/">Infrastructure Services</a></li>
-<li id="menu-item-6255" class="col-sm-6 menu-item menu-item-type-post_type menu-item-object-page menu-item-6255"><a href="https://www.yash.com/it-outsourcing/strategic-consulting/">Strategic Consulting</a></li>
-<li id="menu-item-6256" class="col-sm-6 menu-item menu-item-type-post_type menu-item-object-page menu-item-6256"><a href="https://www.yash.com/it-outsourcing/application-services/enterprise-modernization/">Enterprise Modernization</a></li>
-<li id="menu-item-6257" class="col-sm-6 menu-item menu-item-type-post_type menu-item-object-page menu-item-6257"><a href="https://www.yash.com/technology/sap/cloud-erp/sap-s4-hana/">SAP S/4HANA Cloud</a></li>
-<li id="menu-item-6258" class="col-sm-6 menu-item menu-item-type-post_type menu-item-object-page menu-item-6258"><a href="https://www.yash.com/technology/microsoft/application/office-365/">Office 365</a></li>
-</ul>      </div>
-   </div> 
-
-    <div class="col-sm-3">
-    <div class="f_menu">
-       <h4> Subsidiaries </h4>
-       <ul id="menu-subsidiaries" class=""><li id="menu-item-6259" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-6259"><a href="http://www.itpmconsulting.com/">ITPM Consulting</a></li>
-<li id="menu-item-6260" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-6260"><a href="http://www.soltiusme.com/home/">SoltiusME</a></li>
-<li id="menu-item-6261" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-6261"><a href="http://www.aaseya.com/">Aaseya IT Services Pvt. Ltd.</a></li>
-<li id="menu-item-6262" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-6262"><a href="http://www.codiant.com/">Codiant Technologies</a></li>
-<li id="menu-item-7298" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-7298"><a href="http://www.c5mi.com/">c5mi</a></li>
-</ul>
-    </div>
-
-  
-   </div> 
-
-  <div class="col-sm-3 subscription_box">
-   <h4> Newsletter </h4>
-   <div class="newsletter-title">
-     <p id="newsTitle"> Sign up with your business email address to receive our latest news and updates </p>
-    </div>
-   
-    <div class="row subscription_form">
-     <div class="col-sm-12">
-      <form class="form-inline ysh-newsletter-form has-validation-callback">
-      <input type="hidden" id="hidlocation" name="hid_location" value="http://www.yash.com/">
-        <div class="form-group">
-          <div class="input-group">
-            <input type="email" id="newsLtrEmail" name="nEmail" class="form-control" placeholder="Your Email" data-validation="required custom" data-validation-regexp="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$" data-validation-error-msg="Please enter correct email id">
-            <div class="input-group-addon"><input type="submit" name="Register" class="form-control" value="Subscribe"><button><i class="fa fa-angle-right" aria-hidden="true"></i></button></div>
-          </div>
+        <div className="footer">
+            <div id="section-1" className="section">
+                <h3>Quick Links</h3>
+            </div>
+            <div className="section">
+                <h3>Subsidiaries</h3>
+            </div>
+            <div className="section">
+                <h3>Newsletter</h3>
+            </div>
+            <div>Icons made by <a href="https://www.flaticon.com/authors/freepik" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a> is licensed by <a href="http://creativecommons.org/licenses/by/3.0/" title="Creative Commons BY 3.0" target="_blank" rel="noopener noreferrer">CC 3.0 BY</a></div>
         </div>
-      </form>
-     </div>
-     <div class="newsLetterThankMsg" style="display:none;">Thank you for subscribing to YASH newsletter</div>
-    </div>  
-   
-  </div>
- </div>
- <div class="row">
-   <div class="col-sm-12">  <hr class="f_devider"></div>
- </div>
-
- <div class="row">
-   <div class="col-sm-4">  
-    <p class="copyright">Copyright © 2019. YASH Technologies. All Rights Reserved.</p>
-  </div>
-   <div class="col-sm-4"> 
-     <p class="legal_menu">  
-       <a href="https://www.yash.com/corporate-social-responsibility/"> CSR </a>/
-       <a href="https://www.yash.com/site-map/"> Sitemap </a>/
-       <a href="https://www.yash.com/legal/"> Legal </a>/
-       <a href="https://www.yash.com/privacy-policy/"> Privacy policy </a>
-     </p>
-  </div>
-  <div class="col-sm-4"> 
-    <p class="social_row"> 
-      <a href="https://www.facebook.com/YASHCorporate" class="fb"> <i class="fa fa-facebook" aria-hidden="true"></i> </a>
-      <a href="https://twitter.com/YASH_Tech" class="twitter"> <i class="fa fa-twitter" aria-hidden="true"></i> </a>
-      <a href="https://www.linkedin.com/company/yash-technologies/" class="linkedin"> <i class="fa fa-linkedin" aria-hidden="true"></i> </a>
-     </p>
-  </div>
-  <div>Icons made by <a href="https://www.flaticon.com/authors/freepik" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a> is licensed by <a href="http://creativecommons.org/licenses/by/3.0/" title="Creative Commons BY 3.0" target="_blank" rel="noopener noreferrer">CC 3.0 BY</a></div>
- </div>
-</div>
-</div>
-</div>
-</div>
-</div>
-</section>
-    )
+    );
 }
 
 export default Footer;
