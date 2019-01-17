083617d897ea70e5b6c55e14c887950df9934217
Zachary Karamanlis <zachary.karamanlis@gmail.com>
2019-01-17 10:06:55 -0600

    finshed footer

--- yashadvert/src/components/Footer/Footer.js
+++ yashadvert/src/components/Footer/Footer.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import Sec from '../../hoc/Sec/Sec';
 
+import facebook from "./facebook.svg";
+import twitter from "./twitter.svg";
+import linkedin from "./linkedin.svg";
+
 import './style.css';
 
 const Footer = (props, context) => {
@@ -63,12 +67,34 @@ const Footer = (props, context) => {
             </div>
             <div className="section">
                 <h4>Newsletter</h4>
-                <p>Sign up with your business email address to receive our latest news and updates</p>
-                <input type="text" id="text-box"/><input type="submit" value=">" id="submit-arrow" />
+                <p id="newsletter-p">Sign up with your business email address to receive our latest news and updates</p>
+                <input type="text" id="text-box" placeholder="Your Email"/><input type="submit" value=">" id="submit-arrow" />
             </div>
         </div>
         <div id="lower-footer">
-            <div>Icons made by <a href="https://www.flaticon.com/authors/freepik" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a> is licensed by <a href="http://creativecommons.org/licenses/by/3.0/" title="Creative Commons BY 3.0" target="_blank" rel="noopener noreferrer">CC 3.0 BY</a></div>
+            <div id="line"></div>
+            <div id="lower-container">
+                <div className="lower-footer-third">
+                    <table>
+                        <tbody>
+                            <tr>
+                                <td>Copyright © 2019. YASH Technologies. All Rights Reserved.</td>
+                            </tr>
+                            <tr>
+                                <td><div>Icons made by <a href="https://www.flaticon.com/authors/freepik" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a> is licensed by <a href="http://creativecommons.org/licenses/by/3.0/" title="Creative Commons BY 3.0" target="_blank" rel="noopener noreferrer">CC 3.0 BY</a></div></td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+                <div className="lower-footer-third lower-footer-center">
+                    CSR / Sitemap / Legal / Privacy policy
+                </div>
+                <div className="lower-footer-third lower-footer-center">
+                    <img className="icon" src={facebook} alt="Facebook Icon" />
+                    <img className="icon" src={twitter} alt="Twitter Icon" />
+                    <img className="icon" src={linkedin} alt="LinkedIn Icon" />
+                </div>
+            </div>
         </div>
         </Sec>
     );
