4cffdc97399b6f6c54cb860139e56236e3af74b0
Geon <g-gayles@wiu.edu>
2019-01-18 10:31:31 -0600

    Feat/wahtsnextanimation2
    Reformated the section a little more

---
+++ yashadvert/src/pages/WhatsNext/slider.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+function Slider({src, alt}) {
+  return (
+    
+      <div class="tile">
+      <div class="tile__media">
+        <img class="tile__img" src={src} alt={alt}  />
+      </div>
+      <div class="tile__details">
+        
+      </div>
+    </div>
+    
+  )
+}
+
+
+
+export default Slider;
+
